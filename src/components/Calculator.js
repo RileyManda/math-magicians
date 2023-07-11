84b0c983cd681c4b14ea5d4a2c77e43029a037a3
RileyManda <rileymanda69@gmail.com>
2023-07-11 13:58:18 +0200

    Implemented component constructor

---
+++ src/components/Calculator.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Calcultaor UI </p>
+      </div>
+    );
+  }
+}
+export default Calculator;
