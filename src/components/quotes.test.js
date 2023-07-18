71238e2cf4db923f9e201066c5b3c6bc07ae7157
RileyManda <rileymanda69@gmail.com>
2023-07-18 17:51:17 +0200

    Updated test case to cover fragment snapshot

--- src/components/quotes.test.js
+++ src/components/quotes.test.js
@@ -7,10 +7,11 @@ describe('Quotes', () => {
     const { container } = render(<Quotes />);
     expect(container).toMatchSnapshot();
   });
+
   it('should render error state', () => {
-    const { asFragment } = render(<Quotes />);
     const error = new Error('Failed to fetch data');
     jest.spyOn(global, 'fetch').mockRejectedValueOnce(error);
+    const { asFragment } = render(<Quotes />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
