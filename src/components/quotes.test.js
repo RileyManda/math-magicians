b0f5ddf6dd34f62e2885d3a44089479883815e70
RileyManda <rileymanda69@gmail.com>
2023-07-19 12:26:48 +0200

    Added comments to test

--- src/components/quotes.test.js
+++ src/components/quotes.test.js
@@ -3,11 +3,13 @@ import { render, act } from '@testing-library/react';
 import Quotes from './Quotes';
 
 describe('Quotes', () => {
+  // mock if dom container has been rendered
   it('should match the snapshot', () => {
     const { container } = render(<Quotes />);
     expect(container).toMatchSnapshot();
   });
 
+  // mock if dom container has produced an error to the dom
   it('should render error state', async () => {
     const error = new Error('Failed to fetch data');
     jest.spyOn(global, 'fetch').mockRejectedValueOnce(error);
